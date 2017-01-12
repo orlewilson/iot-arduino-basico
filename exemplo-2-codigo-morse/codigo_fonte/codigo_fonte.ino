95abf1a8f21e499353e52fe51727dd8277480911
Orlewilson B. Maia <orlewilson.maia@gmail.com>
2017-01-11 22:58:16 -0400

    Adição da Prática 2: Código Morse

---
+++ exemplo-2-codigo-morse/codigo_fonte/codigo_fonte.ino
@@ -0,0 +1,73 @@
+/*
+  Curso de Extensao: Programando para IoT com Arduino – Basico
+  Professor:         Dr. Orlewilson Bentes Maia
+  Data:              03/12/2016
+  Descricao:         Alterar o exemplo 01 (criar um Pisca LED) para gerar um codigo Morse (exemplo 02)
+*/
+ 
+// Porta digital no qual sera colocado um LED para ser ligado e desligado
+int led = 13;
+
+// Funcao para ligar o LED
+void ligar(){
+  digitalWrite(led, HIGH);
+}
+
+// Funcao para desligar o LED
+void desligar(){
+  digitalWrite(led, LOW);
+}
+
+// Funcao para simbolizar uma parada curta do codigo morse
+void paradaCurta(){
+  // Funcao do Arduino para parar durante um certo tempo em milisegundos (ms)
+  delay(500);            
+}
+
+// Funcao para simbolizar uma longa curta do codigo morse
+void paradaLonga(){
+  // Funcao do Arduino para parar durante um certo tempo em milisegundos (ms)
+  delay(1000);               
+}
+
+// Funcao para simbolizar uma ponto do codigo morse
+void ponto(){
+  ligar();
+  paradaCurta();
+  desligar();
+  paradaCurta();
+}
+
+// Funcao para simbolizar uma traco do codigo morse
+void traco(){
+  ligar();
+  paradaLonga();
+  desligar();
+  paradaLonga();
+}
+
+// Funcao para simbolizar uma mensagem de SOSO em codigo morse
+void sos(){
+  ponto();
+  ponto();
+  ponto();
+  traco();
+  traco();
+  traco();
+  ponto();
+  ponto();
+  ponto();
+}
+
+// Funcao para configurar o Arduino
+void setup() {                
+
+  // Indica qual porta digital sera utilizada como saida (ligar/desligar um LED)
+  pinMode(led, OUTPUT);     
+}
+
+// Funcao principal do Arduino que ficara em loop infinito
+void loop() {
+  sos();  
+}
+
